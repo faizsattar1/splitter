ea1bf6bd83005076f5e17ceadfba46cdf19995d3
faizsattar1 <faizsattar3@gmail.com>
2021-11-02 14:25:34 +0500

    cool colors

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 "use strict"
+function split4(e){
 
-alert("hello")
+    if(e.shiftKey){
+        //shift is pressed - lets all the children of my parent (that includes me!)
+        this.parentNode.addEventListener("click",split4)
+        this.parentNode.innerHTML=''  //or delete the children a nicer way
+    }
+    else{
+        //Split a div into 4 by adding 4 quarters
+
+       // this.removeEventListener("click",split4)
+        addQuarter(this)
+        addQuarter(this)
+        addQuarter(this)
+        addQuarter(this)
+    }
+
+    // Event 'capturing and bubbling' 
+    e.stopPropagation() //stop the event from firing again on all the ancestor divs
+}
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div")
+
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click", split4)
+
+    intoDiv.appendChild(q)
+}
+
+document.body.addEventListener("click", split4)
